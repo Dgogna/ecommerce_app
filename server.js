6a51cbf8ff9ae7b443d0c2f9bcd7f45f425c748e
Dhruv Gogna <dhruvgogna01@gmail.com>
2023-07-30 22:31:49 +0530

    updated

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url";
 
 
 dotenv.config();
 
 connectDB();
 
+// es module fix
+
 const __filename=fileURLToPath(import.meta.url);
-// >>>>>>> 12bf05028619accd17f22e9dd8a677e0a7833363
 const __dirname=path.dirname(__filename);
 
 const app=express();
@@ -23,6 +26,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 
 // routes
@@ -32,10 +36,12 @@ app.use("/api/v1/product",productRoutes );
 
 
 // api
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to ecommerce app</h1>")
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
 })
+// app.get("/",(req,res)=>{
+//     res.send("<h1>Welcome to ecommerce app</h1>")
+// })
 
 
 const PORT=process.env.PORT || 5000;
