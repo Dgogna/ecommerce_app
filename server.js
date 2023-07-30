6973a1959ebda178b9756e81d0763a6992655482
Dhruv Gogna <dhruvgogna01@gmail.com>
2023-07-30 23:00:44 +0530

    updated

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use("*",function(req,res){
 // })
 
 
-const PORT=process.env.PORT || 8080;
+const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`server is running on part ${PORT}`);
 })
